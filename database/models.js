290ed97871d52c04a658d7c143477393875447e8
Jacob Gramer <grameraggie@gmail.com>
2022-10-01 12:03:09 -0500

    Adds database connection and models

---
+++ database/models.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  accessLevel: String,
+})
+
+const reviewSchema = new mongoose.Schema({
+  user: String,
+  date: Date,
+  body: String,
+  title: String,
+  img: String
+})
+
+const jobSchema = new mongoose.Schema({
+  title: String,
+  address: String,
+  media: String,
+  description: String,
+  categories: [String],
+  date: Date
+})
+
+const toolSchema = new mongoose.Schema({
+  category: String,
+  tools : [String],
+})
+
+module.exports = {
+  Users: mongoose.model('Users', userSchema),
+  Reviews: mongoose.model('Reviews', reviewSchema),
+  Jobs: mongoose.model('Jobs', jobSchema),
+  Tools: mongoose.model('Tools', toolSchema),
+}
+
