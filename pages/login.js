6b6c0084cfe9e373c45d00814ae9afe8ecd5188d
Asael Gonzalez <agonzalezengr@gmail.com>
2022-10-03 15:39:30 -0500

    updated login/logout

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+// function Login() {
+//   return (
+//     <div>
+//       <a href="/api/auth/login">Login</a><br/>
+//       <a href="/api/auth/logout">Logout</a>
+//     </div>
+//   )
+// }
+
+// export default Login
