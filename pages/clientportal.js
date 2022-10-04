a69266e81dc231ef3b227abf6c5fbb629436a822
Adam Polk <adampolk91@gmail.com>
2022-10-04 12:23:50 -0500

    bug fixes from merges

--- pages/clientportal.js
+++ pages/clientportal.js
@@ -7,36 +7,27 @@ import styles from '../styles/Jobs.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-export default function Jobs({ jobs }) {
+export default function Jobs() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return(
     <>
+    {/* <Layout> */}
     <Head>
       <title>Duguid Construction</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <div className={styles.jobList}>
-      <h1 className={styles.title}><span className={styles.username}>Adam's</span> Current Jobs</h1>
-      {jobs.map((job) => (
-        <ClientJobItem job={job} />
-      ))}
+      <h1 className={styles.title}>Adams Current Jobs</h1>
+      <ClientJobItem />
+      <ClientJobItem />
+      <ClientJobItem />
       <button className={styles.addJobBtn} onClick={handleShow}>+ &nbsp;Add a Job</button>
       <JobModal show={show} handleClose={handleClose} />
     </div>
+    {/* </Layout> */}
     </>
   )
-}
-
-export const getStaticProps = async () => {
-  const res = await fetch(`http://localhost:8080/getAllJobs`)
-  const jobs = await res.json()
-
-  return {
-    props: {
-      jobs
-    }
-  }
 }
